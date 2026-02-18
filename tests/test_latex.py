c2ff32ed464c9df8cfc42d6ebc2539028d603f26
Nathan Mull <nmmull@gmail.com>
2026-02-18 07:47:24 -0500

    added regression tests for lin_comb (more to come)

--- tests/test_latex.py
+++ tests/test_latex.py
@@ -1,4 +1,4 @@
-from sympy import *
+from sympy import Matrix
 from stdlib132 import latex
 
 def test_lin_comb_basic(file_regression):
@@ -6,3 +6,22 @@ def test_lin_comb_basic(file_regression):
     result = latex.lin_comb(coeffs)
     file_regression.check(result)
 
+def test_lin_comb_basic_list(file_regression):
+    coeffs = [1, 2, 3, 4, 5]
+    result = latex.lin_comb(coeffs)
+    file_regression.check(result)
+
+def test_lin_comb_basic_row(file_regression):
+    coeffs = Matrix([[1, 2, 3, 4, 5]])
+    result = latex.lin_comb(coeffs)
+    file_regression.check(result)
+
+def test_lin_comb_zero_case(file_regression):
+    coeffs = [0 for _ in range(100)]
+    result = latex.lin_comb(coeffs)
+    file_regression.check(result)
+
+def test_lin_comb_neg_one_first(file_regression):
+    coeffs = [-1, 2, 3, 4, 5]
+    result = latex.lin_comb(coeffs)
+    file_regression.check(result)
